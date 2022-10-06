618110837c3fd51261f4d3cf9f62ea43afd2ff1e
boazglick <95533522+boazglick@users.noreply.github.com>
2022-10-06 20:21:58 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,49 @@
+export default {
+	"pages": {
+		"root": {
+			"name": "root",
+			"children": [
+				"633f0e12f45b7e00247a5c48",
+				"633f0e12f45b7e00247a5c4c",
+				"633f0e12f45b7e00247a5c5c"
+			],
+			"id": "root",
+			"pageUrl": "root"
+		},
+		"633f0e12f45b7e00247a5c48": {
+			"id": "633f0e12f45b7e00247a5c48",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"633f0e12f45b7e00247a5c4c": {
+			"id": "633f0e12f45b7e00247a5c4c",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"title": "Home | Website Example",
+				"og:title": "Home | Website Example",
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		},
+		"633f0e12f45b7e00247a5c5c": {
+			"id": "633f0e12f45b7e00247a5c5c",
+			"pageUrl": "contact",
+			"name": "Contact",
+			"seo": {
+				"og:image": "https://uploads.quarkly.io/5f44d0da669357001e60ed14/images/website-example-quarkly.png?v=2020-11-05T19:48:01.806Z",
+				"title": "Contact | Website Example",
+				"og:title": "Contact | Website Example",
+				"description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.",
+				"og:description": "It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something bigger. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference."
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
